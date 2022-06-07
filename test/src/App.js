b0495fe27a380878241b78e6a2a5ac1acf963fcf
bytrustu <bytrustu@gmail.com>
2022-06-08 00:13:46 +0900

    feat: SummaryPage.test 파일 생성

---
+++ test/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SummaryPage from "./pages/SummaryPage/SummaryPage";
+
+function App() {
+  return (
+    <div className="App">
+      <SummaryPage />
+    </div>
+  );
+}
+
+export default App;
